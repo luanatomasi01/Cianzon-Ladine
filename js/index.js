07b0ddc8ac3b42d042fe16bfac6e687e3a6ee18a
luanatomasi01 <102157922+luanatomasi01@users.noreply.github.com>
2022-05-24 22:01:35 +0200

    search ok + doc finale

--- js/index.js
+++ js/index.js
@@ -1,5 +1,21 @@
-const songs = [
-	{name: 'La cianzon de la Val de Fascia'},
-]
-
-const textimput
+function openpage() {
+	var x = document.getElementById("indian").value.toLowerCase();
+	if (x === "la cianzon de la val de fascia") {
+		window.open("la_cianzon_de_la_val_de_fascia.html","_self")	
+	}
+	if (x === "crepe spavide") {
+		window.open("crepe_spavide.html","_self")
+	}
+	if (x === "bel lengaz") {
+		window.open("bel_lengaz.html","_self")
+	}
+	if (x === "oh ladinia") {
+		window.open("oh_ladinia.html","_self")
+	}
+	if (x === "poltron polka") {
+		window.open("poltron_polka.html","_self")
+	}
+	if (x === "le darmole") {
+		window.open("le_darmole.html","_self")
+	}
+}
